b673489bd443b580b36a006e3ea8c8405798cc45
anand <anandvaibhaw000@gmail.com>
2025-02-23 18:20:04 +0530

    registered todo modeland added rest_frameworks and corsheaders apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo.apps.TodoConfig', # new
+    'corsheaders', # new
+    'rest_framework' # new
 ]
 
 MIDDLEWARE = [
