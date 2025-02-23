a1d6c04907188705cc671f9d454bc7740acf4b5a
anand <anandvaibhaw000@gmail.com>
2025-02-23 17:34:59 +0530

    added todos/ and todos/<int:pk>/ urls

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import TodoListCreateView,TodoDeleteView
 
 urlpatterns = [
-  path("",Home.as_view(),name="home")
+  path("todos/",TodoListCreateView.as_view(),name="todo-list-create"),
+  path("todos/<int:pk>/",TodoDeleteView.as_view(),name="todo-delete"),
 ]
 
